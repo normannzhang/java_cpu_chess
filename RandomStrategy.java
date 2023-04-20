81a3eea50af5f0fda55666cb36ca8e89ed3c4f72
normannzhang <131326996+normannzhang@users.noreply.github.com>
2023-04-19 23:06:46 -0400

    Add files via upload

---
+++ RandomStrategy.java
@@ -0,0 +1,48 @@
+
+import java.lang.Math;
+
+public class RandomStrategy extends Strategy
+{
+	private Piece piece;
+	private int xCoord;
+	private int yCoord;
+	private int orientation;
+	private int pieceIndex;
+
+	RandomStrategy(Game g) {
+		super(g);
+	}
+
+	public void calculateMove(Hand h, int[] score) {
+		boolean legalMove = false;
+		if (game.getCurrentPlayer().checkHand() && game.getCurrentPlayer().getHand().getSize() == 6) {
+			game.getCurrentPlayer().tradeHand();
+		}
+
+		xCoord = (int) (Math.random() * 30);
+		yCoord = (int) (Math.random() * 15);
+		orientation = ((int) (Math.random() * 6));
+		pieceIndex = (int) (Math.random() * h.getSize());
+		piece = h.getPiece(pieceIndex);
+	}
+
+	public int getPieceIndex() {
+		return pieceIndex;
+	}
+
+	public Piece getPiece() {
+		return piece;
+	}
+
+	public int getXCoordinate() {
+		return xCoord;
+	}
+
+	public int getYCoordinate() {
+		return yCoord;
+	}
+
+	public int getOrientation() {
+		return orientation;
+	}
+}
